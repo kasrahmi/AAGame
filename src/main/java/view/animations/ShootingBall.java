a18e5f9046126174e0367f318cd6ae6530fabb95
kasrahmi <a.kasrahmi@gmail.com>
2023-05-20 20:56:33 +0330

    shoot ball with line added

--- src/main/java/view/animations/ShootingBall.java
+++ src/main/java/view/animations/ShootingBall.java
@@ -5,6 +5,7 @@ import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.animation.Transition;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
 import javafx.scene.transform.Rotate;
 import javafx.util.Duration;
 import model.Ball;
@@ -29,7 +30,7 @@ public class ShootingBall extends Transition {
         if (y <= 10) pane.getChildren().remove(ball);
         if (x <= 10 || x >= 590) pane.getChildren().remove(ball);
         if (getDistanceFromCenterOfTheCircle(x, y) <= 200) {
-            rotationBalls(ball);
+            rotationBalls();
             this.stop();
         }
 
@@ -40,12 +41,20 @@ public class ShootingBall extends Transition {
         return Math.sqrt(Math.pow(x - 300, 2) + Math.pow(y - 250, 2));
     }
 
-    public void rotationBalls(Ball ball) {
+    public void rotationBalls() {
         Rotate rotate = new Rotate(0, 300, 250);
         ball.getTransforms().add(rotate);
+        addLine(rotate);
         Timeline timeline = new Timeline(
                 new KeyFrame(Duration.millis(1500 * CurrentGame.getLevel()), new KeyValue(rotate.angleProperty(), 360)));
         timeline.setCycleCount(-1);
         timeline.play();
+    }
+
+    private void addLine(Rotate rotate) {
+        Line line = new Line(ball.getCenterX(), ball.getCenterY() - 15, 300, 355);
+        line.setStrokeWidth(10);
+        line.getTransforms().add(rotate);
+        pane.getChildren().add(line);
     }
 }
